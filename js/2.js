c9788116ed0ee8dbc4db626930fb0f6877fb0221
dougaf0408 <dougaf.santos@gmail.com>
2022-05-30 19:23:52 -0300

    Ataividade 4

---
+++ js/2.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+    $("div p").eq(0).css({ "color": "red" });
+    $("div p").eq(1).css({ "font-size": "20px", "color": "blue" });
+    $("div p").eq(2).css({ "font-size": "30px", "color": "green", "text-align": "center" });
+});
+
+
